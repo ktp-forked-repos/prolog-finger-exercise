1ab4de9cccb6496c6b8b2b3672d75b9e7769e955
Nils Blum-Oeste <nblumoe@dakilla.home>
2015-10-18 10:12:07 +0200

    Add solver for Eight Queens problem

--- day3.pl
+++ day3.pl
@@ -29,3 +29,48 @@ sudoku(Puzzle, Solution) :-
     valid([Row1, Row2, Row3, Row4,
            Col1, Col2, Col3, Col4,
            Square1, Square2, Square3, Square4]).
+
+
+%% 8 queens
+
+valid_queen((Row,Col)) :-
+    Range = [1,2,3,4,5,6,7,8],
+    member(Row, Range),
+    member(Col, Range).
+
+valid_board([]).
+valid_board([Head|Tail]) :-
+    valid_queen(Head),
+    valid_board(Tail).
+
+rows([],[]).
+rows([(Row, _)|QueensTail], [Row|RowsTail]) :-
+    rows(QueensTail, RowsTail).
+
+cols([],[]).
+cols([(_, Col)|QueensTail], [Col|ColsTail]) :-
+    cols(QueensTail, ColsTail).
+
+diags1([],[]).
+diags1([(Row,Col)|QueensTail], [Diagonal|DiagonalTail]) :-
+    Diagonal is Col - Row,
+    diags1(QueensTail, DiagonalTail).
+
+diags2([],[]).
+diags2([(Row,Col)|QueensTail], [Diagonal|DiagonalTail]) :-
+    Diagonal is Col + Row,
+    diags2(QueensTail, DiagonalTail).
+
+
+eight_queens(Queens) :-
+    length(Queens, 8),
+    valid_board(Queens),
+
+    rows(Queens, Rows),
+    cols(Queens, Cols),
+    diags1(Queens, Diags1),
+    diags2(Queens, Diags2),
+    all_different(Rows),
+    all_different(Cols),
+    all_different(Diags1),
+    all_different(Diags2).
